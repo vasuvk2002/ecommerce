d142f48116fb1558c3bf2917d6afcdc8f93b0caa
vasuvk2002 <137818781+vasuvk2002@users.noreply.github.com>
2024-02-12 10:57:02 +0530

    Add files via upload

---
+++ ecommerse/script.js
@@ -0,0 +1,15 @@
+let nav = document.getElementById('navbar')
+let bar = document.getElementById('bar')
+let close = document.getElementById('close')
+
+if (bar) {
+    bar.addEventListener('click' , () =>{
+       nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
